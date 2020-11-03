cf809313c9c0cd38853e40b79f68b3c2a1f621f2
Evangelos Giataganas <e.giataganas@gmail.com>
2020-11-03 16:49:07 +0000

    Add a q as query parameter
    This will allow to search by using either a postcode or an address

--- app/services/properties_search.rb
+++ app/services/properties_search.rb
@@ -4,6 +4,7 @@ class PropertiesSearch
   def initialize(params)
     @address = params.fetch(:address, nil)
     @postcode = params.fetch(:postcode, nil)
+    @q = params.fetch(:q, nil)
   end
 
   def perform
@@ -11,14 +12,15 @@ class PropertiesSearch
 
     return postcode_query if postcode.present?
     return address_query if address.present?
+    return q_query if q.present?
   end
 
 private
 
-  attr_reader :address, :postcode
+  attr_reader :address, :postcode, :q
 
   def valid?
-    address || postcode
+    address || postcode || q
   end
 
   def address_query
@@ -27,5 +29,21 @@ private
 
   def postcode_query
     Property.for_postcode(postcode)
+  end
+
+  def q_query
+    if is_postcode?(q)
+      Property.for_postcode(q)
+    else
+      Property.for_address(q)
+    end
+  end
+
+  def format_postcode(s)
+    s.to_s.gsub(/\s+/, "").upcase
+  end
+
+  def is_postcode?(s)
+    format_postcode(s)[/^[A-Z]{1,2}([0-9]{1,2}|[0-9][A-Z])[0-9][A-Z]{2}$/]
   end
 end

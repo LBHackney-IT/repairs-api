d0e43ace129d228c679995671b3dd93771b0f196
Neil van Beinum <neil.van.beinum@unboxedconsulting.com>
2020-11-05 10:06:49 +0000

    Avoid logging API keys
    Configure Faraday to log using the Rails logger and filter out
    x-api-key values from logs.

--- app/services/connection.rb
+++ app/services/connection.rb
@@ -6,7 +6,10 @@ require "json"
 class Connection
   def self.api(url:, key:)
     Faraday.new(url: url) do |faraday|
-      faraday.response :logger
+      faraday.response :logger, Rails.logger do |logger|
+        logger.filter(/(x-api-key:\s*)\S*$/, '\1[FILTERED]')
+      end
+
       faraday.adapter Faraday.default_adapter
       faraday.headers["Content-Type"] = "application/json"
       faraday.headers["x-api-key"] = key

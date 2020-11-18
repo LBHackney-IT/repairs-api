309d4a4acf4bf4ab6daefac5fd2bbd83227ca548
Evangelos Giataganas <e.giataganas@gmail.com>
2020-11-18 12:51:58 +0000

    Add ApiClient table which will be used for API Authentication

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 2020_11_18_125001) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "api_clients", force: :cascade do |t|
+    t.string "client_id", null: false
+    t.string "client_secret", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["client_id"], name: "index_api_clients_on_client_id", unique: true
+    t.index ["client_secret"], name: "index_api_clients_on_client_secret", unique: true
+  end
 
 end

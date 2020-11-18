2a7ad2af01d1f9b2abffbca39807e5c86e101822
Evangelos Giataganas <e.giataganas@gmail.com>
2020-11-18 17:26:11 +0000

    Add authenticate_token method for each endpoint

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,21 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::API
+  before_action :authenticate_token!
+
+  def authenticate_token!
+    payload = JsonWebToken.decode(auth_token)
+    @current_api_client = ApiClient.find_by(
+      client_id: payload["client_id"],
+      client_secret: payload["client_secret"]
+    )
+  rescue JWT::DecodeError
+    render json: { errors: "Invalid auth token" }, status: :unauthorized
+  end
+
+private
+
+  def auth_token
+    @auth_token ||= request.headers.fetch("Authorization", "").split(" ").last
+  end
 end

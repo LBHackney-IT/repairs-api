acbf5fdb2c5fb20b04c83ddb2f57d218560c882e
Ben <benbaumann95@hotmail.co.uk>
2020-12-14 15:27:49 +0000

    Return empty array when no tenancy agreement ref is found - The tenancy information platform API always returns a 200 response but response["tenancies"] can be empty so no tenancy agreement reference is found. This means that there won't be any person alerts on the property. To be consistent we just return an empty array in this scenario

--- app/models/person_alert.rb
+++ app/models/person_alert.rb
@@ -7,7 +7,7 @@ class PersonAlert < Alert
     def for_resident_alerts(property_reference)
       response = for_tenancy_reference(find_tenancy_reference(property_reference))
 
-      build(response["contacts"].first["alerts"])
+      build(response)
     end
 
   private
@@ -15,11 +15,17 @@ class PersonAlert < Alert
     def find_tenancy_reference(reference)
       response = PlatformApis::TenancyInformation::Client.get_tenancy_information_by_property_reference(reference)
 
-      response["tenancies"].first["tenancyAgreementReference"]
+      # The API returns a 200 with response "tenancies": [] when no
+      # tenancy agreement reference can be found for a property.
+      # In this case we will return nil if response["tenancies"] is empty
+      response["tenancies"].first["tenancyAgreementReference"] if response["tenancies"].any?
     end
 
     def for_tenancy_reference(reference)
-      PlatformApis::Alerts::Client.get_alerts_by_tenancy_reference(reference)
+      # Return empty array when no tenancy agreement reference has been found
+      return [] unless reference
+
+      PlatformApis::Alerts::Client.get_alerts_by_tenancy_reference(reference)["contacts"].first["alerts"]
     end
   end
 end
